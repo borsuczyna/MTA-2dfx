9741c615aa06c64ccb7e2ee499ad5ed312fc8430
borsuczyna <65825775+borsuczyna@users.noreply.github.com>
2022-04-13 20:28:29 +0300

    1.1
    Major optimization made by thisdp

--- data/corona.lua
+++ data/corona.lua
@@ -7,6 +7,9 @@ dxSetShaderValue(shader, "gCoronaTexture", coronaTxt)
 function createCorona(x, y, z, size, color, type)
     table.insert(coronas, {
         pos = Vector3(x, y, z),
+		posX = x,
+		posY = y,
+		posZ = z,
         size = size,
         color = color,
         type = type,
@@ -23,17 +26,10 @@ function pointInTriangle(x1, y1, x2, y2, x3, y3, x, y)
     return (a >= 0 and a <= 1) and (b >= 0 and b <= 1) and (c >= 0 and c <= 1)
 end
 
-function getPositionFromElementOffset2(m,offX,offY,offZ)
-    local x = offX * m[1][1] + offY * m[2][1] + offZ * m[3][1] + m[4][1]
-    local y = offX * m[1][2] + offY * m[2][2] + offZ * m[3][2] + m[4][2]
-    local z = offX * m[1][3] + offY * m[2][3] + offZ * m[3][3] + m[4][3]
-    return x, y, z
-end
-
 function cameraView()
     local matrix = getElementMatrix(getCamera())
-    local x1, y1, z1 = getPositionFromElementOffset2(matrix, -1350, 1800, 0)
-    local x2, y2, z2 = getPositionFromElementOffset2(matrix, 1350, 1800, 0)
+    local x1, y1, z1 = getPositionFromElementOffset(matrix, -1350, 1800, 0)
+    local x2, y2, z2 = getPositionFromElementOffset(matrix, 1350, 1800, 0)
     local x3, y3, z3 = matrix[4][1], matrix[4][2], matrix[4][3]
     return {
         {x1, y1, z3},
@@ -68,75 +64,57 @@ function nightTime()
     end
 end
 
+local getScreenFromWorldPosition = getScreenFromWorldPosition
+local dxDrawMaterialLine3D = dxDrawMaterialLine3D
+local dxSetShaderValue = dxSetShaderValue
+local getDistanceBetweenPoints3D = getDistanceBetweenPoints3D
+local alphaType = {[0]=255,255,255,255,255,255}
+
 function drawCorona(x, y, z, size, color, type)
     local night = nightTime()
     local dist = getDistanceBetweenPoints3D(cameraPosition, x, y, z)   
-    local alpha = 1
-    if dist < 180 then
-        alpha = math.max((dist-90)/90, 0.2)
-    end
-
-    local type2alpha = interpolateBetween(0, 0, 0, 1, 0, 0, (getTickCount()%1500)/1500, "CosineCurve")
-    local type3alpha = interpolateBetween(0, 0, 0, 1, 0, 0, (getTickCount()%3500)/3500, "CosineCurve")
-    
+	local distRatio = dist/400
+	local alpha = distRatio < 1 and distRatio or 1
     if getScreenFromWorldPosition(x, y, z) and dist < 900 then
-        if type == 0 then
-        elseif type == 2 then
-            alpha = alpha*type2alpha
-        elseif type == 3 then
-            alpha = alpha*type3alpha
-        else
-            alpha = alpha*type3alpha
-        end
-
+		alpha = alpha*alphaType[type]*night
         dxSetShaderValue(shader, "fCoronaPosition", x, y, z)
-        dxDrawMaterialLine3D(x, y, z - size * 2,
-        x, y, z + size * 2,
-        shader, size * 4, tocolor(color[1], color[2], color[3], alpha * 255 * night),
-        x,1 + y,z)
+        dxDrawMaterialLine3D(
+			x, y, z-size*2,
+			x, y, z+size*2,
+			shader, size*4, color+(alpha-alpha%1)*0x1000000,
+			x, y+1, z
+		)
     end
 end
 
 addEventHandler("onClientPreRender", root, function()
-    cameraPosition = Vector3(getCameraMatrix())
-
-    local view = cameraView()
+	local tick = getTickCount()
     local night = nightTime()
-    --[[dxDrawLine3D(view[1][1], view[1][2], view[1][3], view[2][1], view[2][2], view[2][3], tocolor(255, 0, 0, 255), 2, true)
-    dxDrawLine3D(view[2][1], view[2][2], view[2][3], view[3][1], view[3][2], view[3][3], tocolor(255, 0, 0, 255), 2, true)
-    dxDrawLine3D(view[3][1], view[3][2], view[3][3], view[1][1], view[1][2], view[1][3], tocolor(255, 0, 0, 255), 2, true)]]
-
-    local type2alpha = interpolateBetween(0, 0, 0, 1, 0, 0, (getTickCount()%1500)/1500, "CosineCurve")
-    local type3alpha = interpolateBetween(0, 0, 0, 1, 0, 0, (getTickCount()%3500)/3500, "CosineCurve")
-
-    for k,v in pairs(coronas) do
-        local pos = v.pos
-        local size = v.size/2
-        local color = v.color
-
-        local dist = getDistanceBetweenPoints3D(pos, cameraPosition)
-        local alpha = 1
-        if dist < 180 then
-            alpha = math.max((dist-90)/90, 0.2)
-        end
-        size = size * alpha
-
-        if v.type == 0 then
-        elseif v.type == 2 then
-            alpha = alpha*type2alpha
-        elseif v.type == 3 then
-            alpha = alpha*type3alpha
-        else
-            alpha = alpha*type3alpha
-        end
-
-        if dist < 900 and getScreenFromWorldPosition(pos.x, pos.y, pos.z) then
-            dxSetShaderValue(shader, "fCoronaPosition", pos.x, pos.y, pos.z)
-
-            dxDrawMaterialLine3D(pos.x, pos.y, pos.z - size * 2,
-            pos.x, pos.y, pos.z + size * 2,
-            shader, size * 4, tocolor(color[1], color[2], color[3], alpha * 255 * night),
-            pos.x,1 + pos.y,pos.z)
+    alphaType[2] = interpolateBetween(0, 0, 0, 255, 0, 0, (tick%1500)/1500, "CosineCurve")*night
+    alphaType[3] = interpolateBetween(0, 0, 0, 255, 0, 0, (tick%3500)/3500, "CosineCurve")*night
+    alphaType[4] = alphaType[3]
+    alphaType[6] = alphaType[3]
+    local cx,cy,cz = getCameraMatrix()
+    local view = cameraView()
+    for i=1,#coronas do
+		local v = coronas[i]
+		local x,y,z = v.posX,v.posY,v.posZ
+        local dist = ((x-cx)^2+(y-cy)^2+(z-cz)^2)^0.5
+        if dist < 900 then
+			local pos = v.pos
+			if getScreenFromWorldPosition(pos) then
+				local distRatio = dist/400
+				local alpha = distRatio < 1 and distRatio or 1
+				local size = v.size*alpha
+				alpha = alpha*alphaType[v.type]*night
+				dxSetShaderValue(shader,"fCoronaPosition",pos)
+				dxDrawMaterialLine3D(
+					x, y, z-size,
+					x, y, z+size,
+					shader, size*2, v.color+(alpha-alpha%1)*0x1000000,
+					x, y+1, z
+				)
+			end
         end
     end
 end)
